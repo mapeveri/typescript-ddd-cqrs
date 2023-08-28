a0f0e236008eae1a519ed3aaa7157438a3eccfaf
Martín Peveri <martinpeveri@gmail.com>
2023-08-28 19:43:57 +0200

    Remove unused types

--- src/shared/infrastructure/types/express/index.d.ts
+++ src/shared/infrastructure/types/express/index.d.ts
@@ -1,10 +1,5 @@
 declare namespace Express {
-  export interface Container {
-    get(service: string): any;
-  }
-
   export interface Request {
-    container: Container;
     user?: { id: string, name: string, email: string };
   }
 }
