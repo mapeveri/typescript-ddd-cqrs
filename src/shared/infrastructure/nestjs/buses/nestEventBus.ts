68b0c2dd137e2795d62c7a27c889ef5162630190
Martín Peveri <martinpeveri@gmail.com>
2023-09-26 18:23:38 +0200

    Subscriber backoff #38

--- src/shared/infrastructure/nestjs/buses/nestEventBus.ts
+++ src/shared/infrastructure/nestjs/buses/nestEventBus.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { EventBus } from '@nestjs/cqrs';
 import { DomainEvent } from '@src/shared/domain/buses/eventBus/domainEvent';
 import { EventBus as IEventBus } from '@src/shared/domain/buses/eventBus/eventBus';
+import { backOff } from 'exponential-backoff';
 
 @Injectable()
 export default class NestEventBus implements IEventBus {
   constructor(private eventBus: EventBus) {}
 
   publish(events: DomainEvent[]): void {
-    void this.eventBus.publishAll(events);
+    void backOff(() => this.eventBus.publishAll(events));
   }
 }
