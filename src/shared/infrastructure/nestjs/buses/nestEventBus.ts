d019d816700ca1d8911d2afb59238d6437971f31
Martín Peveri <martinpeveri@gmail.com>
2023-10-01 12:56:54 +0200

    Modify nestEventBus decorator #38

--- src/shared/infrastructure/nestjs/buses/nestEventBus.ts
+++ src/shared/infrastructure/nestjs/buses/nestEventBus.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { EventBus } from '@nestjs/cqrs';
 import { DomainEvent } from '@src/shared/domain/buses/eventBus/domainEvent';
 import { EventBus as IEventBus } from '@src/shared/domain/buses/eventBus/eventBus';
-import { backOff } from 'exponential-backoff';
 
 @Injectable()
 export default class NestEventBus implements IEventBus {
   constructor(private eventBus: EventBus) {}
 
   publish(events: DomainEvent[]): void {
-    void backOff(() => this.eventBus.publishAll(events), {numOfAttempts: 3});
+    void this.eventBus.publishAll(events);
   }
 }
