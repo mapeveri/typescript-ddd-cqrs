9ce32fde4cbeffe51b955db072d7d379b8ba132c
Martín Peveri <martinpeveri@gmail.com>
2023-12-13 09:21:13 +0100

    Add a better way to get the user id using nest.js #69

--- src/languages/infrastructure/ui/api/v1/controllers/auth/meGetController.ts
+++ src/languages/infrastructure/ui/api/v1/controllers/auth/meGetController.ts
@@ -1,6 +1,6 @@
 import { Request } from 'express';
 import FindUserQuery from '@src/languages/application/user/query/find/findUserQuery';
-import { Controller, Get, HttpCode, HttpException, HttpStatus, Inject, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, Inject, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '@src/shared/infrastructure/nestjs/guards/JwtAuthGuard';
 import MeGetResponseDto from './meGetResponseDto';
 import {
@@ -24,12 +24,8 @@ export default class MeGetController {
   @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
   @ApiInternalServerErrorResponse({ description: 'Internal Server Error.' })
   @UseGuards(JwtAuthGuard)
-  async run(@Req() request: Request): Promise<MeGetResponseDto> {
-    const userId = request.user?.id;
-    if (!userId) {
-      throw new HttpException('Invalid user', HttpStatus.FORBIDDEN);
-    }
-
+  async run(@Req() req: Request): Promise<MeGetResponseDto> {
+    const userId = req.user['id'];
     const data = await this.queryBus.ask(new FindUserQuery(userId));
 
     return data.content;
