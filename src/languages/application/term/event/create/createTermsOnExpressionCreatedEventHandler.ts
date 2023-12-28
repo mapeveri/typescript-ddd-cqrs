eb74be56363d3bb095517243036de858e6530807
Martín Peveri <martinpeveri@gmail.com>
2023-12-28 17:56:34 +0100

    Chore: Refactor event handler names

--- src/languages/application/term/event/create/createTermsOnExpressionCreatedEventHandler.ts
+++ src/languages/application/term/event/create/createTermsOnExpressionCreatedEventHandler.ts
@@ -1,24 +1,24 @@
-import WordCreatedEvent from '@src/languages/domain/word/domainEvents/wordCreatedEvent';
 import CreateTermProjection from '@src/languages/application/term/projection/create/createTermProjection';
+import ExpressionCreatedEvent from '@src/languages/domain/expression/domainEvents/expressionCreatedEvent';
 import { Inject } from '@src/shared/domain/injector/inject.decorator';
 import { EventsHandler, IEventHandler } from '@src/shared/domain/buses/eventBus/eventsHandler';
 import { TermTypeEnum } from '@src/languages/domain/term/valueObjects/termType';
 import { PROJECTION_BUS, ProjectionBus } from '@src/shared/domain/buses/projectionBus/projectionBus';
 
-@EventsHandler(WordCreatedEvent)
-export default class CreateOnWordCreatedEventHandler implements IEventHandler<WordCreatedEvent> {
+@EventsHandler(ExpressionCreatedEvent)
+export default class CreateTermsOnExpressionCreatedEventHandler implements IEventHandler<ExpressionCreatedEvent> {
   constructor(@Inject(PROJECTION_BUS) private readonly projectionBus: ProjectionBus) {}
 
-  async handle(event: WordCreatedEvent): Promise<void> {
+  async handle(event: ExpressionCreatedEvent): Promise<void> {
     for (const term of event.terms) {
       await this.projectionBus.dispatch(
         new CreateTermProjection(
           event.aggregateId,
-          term['word'],
+          term['expression'],
           term['description'],
           term['example'],
           term['hashtags'],
-          TermTypeEnum.WORD,
+          TermTypeEnum.EXPRESSION,
         ),
       );
     }
--- src/languages/application/term/event/create/createTermsOnWordCreatedEventHandler.ts
+++ src/languages/application/term/event/create/createTermsOnWordCreatedEventHandler.ts
@@ -1,24 +1,24 @@
+import WordCreatedEvent from '@src/languages/domain/word/domainEvents/wordCreatedEvent';
 import CreateTermProjection from '@src/languages/application/term/projection/create/createTermProjection';
-import ExpressionCreatedEvent from '@src/languages/domain/expression/domainEvents/expressionCreatedEvent';
 import { Inject } from '@src/shared/domain/injector/inject.decorator';
 import { EventsHandler, IEventHandler } from '@src/shared/domain/buses/eventBus/eventsHandler';
 import { TermTypeEnum } from '@src/languages/domain/term/valueObjects/termType';
 import { PROJECTION_BUS, ProjectionBus } from '@src/shared/domain/buses/projectionBus/projectionBus';
 
-@EventsHandler(ExpressionCreatedEvent)
-export default class CreateOnExpressionCreatedEventHandler implements IEventHandler<ExpressionCreatedEvent> {
+@EventsHandler(WordCreatedEvent)
+export default class CreateTermsOnWordCreatedEventHandler implements IEventHandler<WordCreatedEvent> {
   constructor(@Inject(PROJECTION_BUS) private readonly projectionBus: ProjectionBus) {}
 
-  async handle(event: ExpressionCreatedEvent): Promise<void> {
+  async handle(event: WordCreatedEvent): Promise<void> {
     for (const term of event.terms) {
       await this.projectionBus.dispatch(
         new CreateTermProjection(
           event.aggregateId,
-          term['expression'],
+          term['word'],
           term['description'],
           term['example'],
           term['hashtags'],
-          TermTypeEnum.EXPRESSION,
+          TermTypeEnum.WORD,
         ),
       );
     }
