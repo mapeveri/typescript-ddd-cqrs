e9a835b8d249771f87cc9b033acb608d0e6485d7
Martín Peveri <martinpeveri@gmail.com>
2024-01-11 21:29:58 +0100

    Add sort in term repository

--- src/languages/domain/term/termCriteria.ts
+++ src/languages/domain/term/termCriteria.ts
@@ -1,12 +1,15 @@
+type Order = { key: string; order: 'desc' | 'asc' };
+
 export default class TermCriteria {
   constructor(
     public readonly size: number,
     public readonly page: number,
     public readonly term?: string,
     public readonly hashtags?: string[],
+    public readonly orderBy?: Order,
   ) {}
 
-  static from(params: { size: number; page: number; term?: string; hashtags?: string[] }) {
-    return new TermCriteria(params.size, params.page, params.term, params.hashtags);
+  static from(params: { size: number; page: number; term?: string; hashtags?: string[]; orderBy?: Order }) {
+    return new TermCriteria(params.size, params.page, params.term, params.hashtags, params.orderBy);
   }
 }
