5c62558877c7f299d06e5a79cad9e272994b9bd5
Martín Peveri <martinpeveri@gmail.com>
2024-01-21 20:35:11 +0100

    Move readLayer folder

--- src/languages/_dependencyInjection/readLayers.ts
+++ src/languages/_dependencyInjection/readLayers.ts
@@ -1,5 +1,5 @@
 import { FIND_SUGGESTIONS_TERM_READ_LAYER } from '@src/languages/application/term/query/suggestion/findSuggestionsTermReadLayer';
-import MongoFindSuggestionsTermReadLayer from '@src/languages/infrastructure/readLayer/mongoFindSuggestionsTermReadLayer';
+import MongoFindSuggestionsTermReadLayer from '@src/languages/infrastructure/persistence/mongo/readLayer/mongoFindSuggestionsTermReadLayer';
 
 export const readLayers = [
   {
