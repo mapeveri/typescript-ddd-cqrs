8b861ed4760456a2c7712ae346213add484ea610
Martín Peveri <martinpeveri@gmail.com>
2024-02-01 20:20:15 +0100

    Move expression and word controllers to term folder

--- src/languages/_dependencyInjection/controllers.ts
+++ src/languages/_dependencyInjection/controllers.ts
@@ -3,8 +3,8 @@ import RefreshTokenPostController from '@src/languages/infrastructure/api/v1/con
 import MeGetController from '@src/languages/infrastructure/api/v1/controllers/auth/meGetController';
 import SearchTermsGetController from '@src/languages/infrastructure/api/v1/controllers/terms/searchTermsGetController';
 import FindSuggestionsTermGetController from '@src/languages/infrastructure/api/v1/controllers/terms/findSuggestionsTermGetController';
-import WordPostController from '@src/languages/infrastructure/api/v1/controllers/words/wordPostController';
-import ExpressionPostController from '@src/languages/infrastructure/api/v1/controllers/expressions/expressionPostController';
+import WordPostController from '@src/languages/infrastructure/api/v1/controllers/terms/words/wordPostController';
+import ExpressionPostController from '@src/languages/infrastructure/api/v1/controllers/terms/expressions/expressionPostController';
 import CountriesGetController from '@src/languages/infrastructure/api/v1/controllers/countries/countriesGetController';
 import CountryGetController from '@src/languages/infrastructure/api/v1/controllers/countries/countryGetController';
 import CountryPostController from '@src/languages/infrastructure/api/v1/controllers/countries/countryPostController';
