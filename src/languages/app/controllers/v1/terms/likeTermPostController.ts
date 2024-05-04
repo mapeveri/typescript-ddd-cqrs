20302cd8a783fc6b36dcd6797799e6ac56b84a31
Martín Peveri <martinpeveri@gmail.com>
2024-05-04 12:40:52 +0200

    feat: 112 Add like post controller

---
+++ src/languages/app/controllers/v1/terms/likeTermPostController.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { NestJwtAuthGuard } from '@src/shared/guards/nestJwtAuthGuard';
+import {
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import LikeTermPostDto from '@src/languages/app/controllers/v1/terms/likeTermPostDto';
+
+@ApiTags('terms')
+@Controller()
+export default class WordPostController {
+  @Post('terms')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({ description: 'The record has been successfully created.' })
+  @ApiBadRequestResponse({ description: 'Bad Request.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal Server Error.' })
+  @UseGuards(NestJwtAuthGuard)
+  async run(@Body() _payload: LikeTermPostDto): Promise<any> {
+    return Promise.resolve();
+  }
+}
