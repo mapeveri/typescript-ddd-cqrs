6321ed1da3339778bf79a314f2a33be0bc5d141d
Martín Peveri <martinpeveri@gmail.com>
2024-05-11 16:43:39 +0200

    Added AddLikeToTermProjectionHandler

--- src/languages/_dependencyInjection/projectionHandlers.ts
+++ src/languages/_dependencyInjection/projectionHandlers.ts
@@ -1,3 +1,4 @@
 import CreateTermProjectionHandler from '@src/languages/application/term/projection/createTermProjectionHandler';
+import AddLikeToTermProjectionHandler from '@src/languages/application/term/projection/addLikeToTermProjectionHandler';
 
-export const projections = [CreateTermProjectionHandler];
+export const projections = [CreateTermProjectionHandler, AddLikeToTermProjectionHandler];
--- src/languages/application/term/event/updateTermsOnTermLikeAddedEventHandler.ts
+++ src/languages/application/term/event/updateTermsOnTermLikeAddedEventHandler.ts
@@ -1,9 +1,14 @@
 import { EventsHandler, IEventHandler } from '@src/shared/domain/bus/eventBus/eventsHandler';
 import TermLikeAddedEvent from '@src/languages/domain/term/termLikeAddedEvent';
+import { Inject } from '@src/shared/domain/injector/inject.decorator';
+import { PROJECTION_BUS, ProjectionBus } from '@src/shared/domain/bus/projectionBus/projectionBus';
+import AddLikeToTermProjection from '@src/languages/application/term/projection/addLikeToTermProjection';
 
 @EventsHandler(TermLikeAddedEvent)
 export default class UpdateTermsOnTermLikeAddedEventHandler implements IEventHandler<TermLikeAddedEvent> {
+  constructor(@Inject(PROJECTION_BUS) private readonly projectionBus: ProjectionBus) {}
+
   async handle(event: TermLikeAddedEvent): Promise<void> {
-    console.log(event);
+    await this.projectionBus.dispatch(new AddLikeToTermProjection(event.id, event.userId, event.name, event.photo));
   }
 }
