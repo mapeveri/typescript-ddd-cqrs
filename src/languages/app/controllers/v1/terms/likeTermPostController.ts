cdcbd25ef8e1f08002f0c749736daf273b8b575f
Martín Peveri <martinpeveri@gmail.com>
2024-05-13 20:37:09 +0200

    Changed likeTermPostController

--- src/languages/app/controllers/v1/terms/likeTermPostController.ts
+++ src/languages/app/controllers/v1/terms/likeTermPostController.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Inject, Post, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Inject, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { NestJwtAuthGuard } from '@src/shared/guards/nestJwtAuthGuard';
 import {
   ApiCreatedResponse,
@@ -7,23 +7,24 @@ import {
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
-import LikeTermPostDto from '@src/languages/app/controllers/v1/terms/likeTermPostDto';
 import { COMMAND_BUS, CommandBus } from '@src/shared/domain/bus/commandBus/commandBus';
 import AddLikeTermCommand from '@src/languages/application/term/command/addLikeTermCommand';
+import { Request } from 'express';
 
 @ApiTags('terms')
 @Controller()
 export default class LikeTermPostController {
   constructor(@Inject(COMMAND_BUS) private commandBus: CommandBus) {}
 
-  @Post('terms')
+  @Post('terms/:id/add-like')
   @HttpCode(HttpStatus.CREATED)
   @ApiCreatedResponse({ description: 'The record has been successfully created.' })
   @ApiBadRequestResponse({ description: 'Bad Request.' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
   @ApiInternalServerErrorResponse({ description: 'Internal Server Error.' })
   @UseGuards(NestJwtAuthGuard)
-  async run(@Body() payload: LikeTermPostDto): Promise<any> {
-    await this.commandBus.dispatch(new AddLikeTermCommand(payload.termId, payload.userId));
+  async run(@Param('id') id: string, @Req() req: Request): Promise<any> {
+    const userId = req.user['id'];
+    await this.commandBus.dispatch(new AddLikeTermCommand(id, userId));
   }
 }
