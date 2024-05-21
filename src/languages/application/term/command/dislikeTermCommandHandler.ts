be90f47fc064f255edf1d277769a65e9cb243dc8
Martín Peveri <martinpeveri@gmail.com>
2024-05-21 20:33:02 +0200

    Added get term and user

--- src/languages/application/term/command/dislikeTermCommandHandler.ts
+++ src/languages/application/term/command/dislikeTermCommandHandler.ts
@@ -2,11 +2,46 @@ import { CommandHandler, ICommandHandler } from '@src/shared/domain/bus/commandB
 import TermId from '@src/languages/domain/term/termId';
 import UserId from '@src/languages/domain/user/userId';
 import DislikeTermCommand from '@src/languages/application/term/command/dislikeTermCommand';
+import Term from '@src/languages/domain/term/term';
+import TermDoesNotExistsException from '@src/languages/domain/term/termDoesNotExistsException';
+import User from '@src/languages/domain/user/user';
+import UserDoesNotExistsException from '@src/languages/domain/user/userDoesNotExistsException';
+import TermLike from '@src/languages/domain/term/termLike';
+import { Inject } from '@src/shared/domain/injector/inject.decorator';
+import TermRepository, { TERM_REPOSITORY } from '@src/languages/domain/term/termRepository';
+import UserRepository, { USER_REPOSITORY } from '@src/languages/domain/user/userRepository';
 
 @CommandHandler(DislikeTermCommand)
 export default class DislikeTermCommandHandler implements ICommandHandler<DislikeTermCommand> {
+  constructor(
+    @Inject(TERM_REPOSITORY) private readonly termRepository: TermRepository,
+    @Inject(USER_REPOSITORY) private readonly userRepository: UserRepository,
+  ) {}
+
   async execute(command: DislikeTermCommand): Promise<void> {
-    TermId.of(command.termId);
-    UserId.of(command.userId);
+    const termId = TermId.of(command.termId);
+    const userId = UserId.of(command.userId);
+
+    await this.getTerm(termId);
+    const user = await this.getUser(userId);
+    TermLike.of({ userId: userId.toString(), name: user.name, photo: user.photo });
+  }
+
+  private async getTerm(termId: TermId): Promise<Term> {
+    const term = await this.termRepository.findById(termId);
+    if (!term) {
+      throw new TermDoesNotExistsException(termId.toString());
+    }
+
+    return term;
+  }
+
+  private async getUser(userId: UserId): Promise<User> {
+    const user = await this.userRepository.findById(userId);
+    if (!user) {
+      throw new UserDoesNotExistsException(userId.toString());
+    }
+
+    return user;
   }
 }
--- test/unit/languages/application/term/command/dislikeTermCommandHandler.test.ts
+++ test/unit/languages/application/term/command/dislikeTermCommandHandler.test.ts
@@ -3,12 +3,25 @@ import InvalidArgumentException from '@src/shared/domain/exceptions/invalidArgum
 import DislikeTermCommandHandler from '@src/languages/application/term/command/dislikeTermCommandHandler';
 import { DislikeTermCommandMother } from '@test/unit/languages/application/term/command/dislikeTermCommandMother';
 import DislikeTermCommand from '@src/languages/application/term/command/dislikeTermCommand';
+import { TermRepositoryMock } from '@test/unit/languages/domain/term/termRepositoryMock';
+import { UserRepositoryMock } from '@test/unit/languages/domain/user/userRepositoryMock';
+import TermDoesNotExistsException from '@src/languages/domain/term/termDoesNotExistsException';
+import WordMother from '@test/unit/languages/domain/term/word/wordMother';
+import { TermIdMother } from '@test/unit/languages/domain/term/termIdMother';
+import UserDoesNotExistsException from '@src/languages/domain/user/userDoesNotExistsException';
 
 describe('Given a DislikeTermCommandHandler', () => {
+  const USER_ID = '0a8008d5-ab68-4c10-8476-668b5b540e0f';
+  const TERM_ID = '7abe3a96-d603-4e87-b69e-e9fb372294de';
+
+  let termRepository: TermRepositoryMock;
+  let userRepository: UserRepositoryMock;
   let handler: DislikeTermCommandHandler;
 
   beforeEach(() => {
-    handler = new DislikeTermCommandHandler();
+    termRepository = new TermRepositoryMock();
+    userRepository = new UserRepositoryMock();
+    handler = new DislikeTermCommandHandler(termRepository, userRepository);
 
     jest.useFakeTimers();
   });
@@ -38,6 +51,37 @@ describe('Given a DislikeTermCommandHandler', () => {
 
     it('then should thrown an exception', async () => {
       await expect(handler.execute(command)).rejects.toThrowError(InvalidArgumentException);
+    });
+  });
+
+  describe('When the term does not exists ', () => {
+    let command: DislikeTermCommand;
+
+    function startScenario() {
+      command = DislikeTermCommandMother.random();
+    }
+
+    beforeEach(startScenario);
+
+    it('then should thrown an exception', async () => {
+      await expect(handler.execute(command)).rejects.toThrowError(TermDoesNotExistsException);
+    });
+  });
+
+  describe('When the user does not exists ', () => {
+    let command: DislikeTermCommand;
+
+    function startScenario() {
+      command = DislikeTermCommandMother.random({ termId: TERM_ID, userId: USER_ID });
+      const term = WordMother.random({ id: TermIdMother.random(TERM_ID) });
+
+      termRepository.add(term);
+    }
+
+    beforeEach(startScenario);
+
+    it('then should thrown an exception', async () => {
+      await expect(handler.execute(command)).rejects.toThrowError(UserDoesNotExistsException);
     });
   });
 });
