f7a9c3e398c010053e97bacb0183e30bfa8079a2
Martín Peveri <martinpeveri@gmail.com>
2024-05-29 22:01:09 +0200

    Added google oauth client to DI

--- src/languages/infrastructure/oauth/googleSocialAuthenticator.ts
+++ src/languages/infrastructure/oauth/googleSocialAuthenticator.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { SocialAuthenticator } from '@src/languages/domain/auth/socialAuthenticator';
 import { OAuth2Client } from 'google-auth-library';
-import { ConfigService } from '@nestjs/config';
 import { Inject } from '@src/shared/domain/injector/inject.decorator';
 import Logger, { LOGGER } from '@src/shared/domain/logger';
 
 @Injectable()
 export default class GoogleSocialAuthenticator implements SocialAuthenticator {
-  private client: OAuth2Client;
-  private readonly clientId: string | undefined;
-
-  constructor(readonly configService: ConfigService, @Inject(LOGGER) private readonly logger: Logger) {
-    this.clientId = this.configService.get<string>('GOOGLE_CLIENT_ID');
-    this.client = new OAuth2Client(this.clientId);
-  }
+  constructor(
+    @Inject('GOOGLE_OAUTH_CLIENT') readonly client: OAuth2Client,
+    @Inject(LOGGER) private readonly logger: Logger,
+  ) {}
 
   async login(token: string): Promise<boolean> {
     let isValid;
     try {
       await this.client.verifyIdToken({
         idToken: token,
-        audience: this.clientId,
+        audience: this.client._clientId,
       });
 
       isValid = true;
