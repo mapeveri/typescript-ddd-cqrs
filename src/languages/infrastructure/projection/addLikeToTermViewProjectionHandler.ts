54c1cdca4a43e9bd05185aeb837203596049a467
Martín Peveri <martinpeveri@gmail.com>
2024-06-03 21:32:40 +0200

    Fixed AddLikeToTermViewProjectionHandler 132

--- src/languages/infrastructure/projection/addLikeToTermViewProjectionHandler.ts
+++ src/languages/infrastructure/projection/addLikeToTermViewProjectionHandler.ts
@@ -3,7 +3,6 @@ import MongoConnection, { MONGO_CLIENT } from '@src/shared/infrastructure/persis
 import { TermLike, TermView } from '@src/languages/application/term/view/termView';
 import { Collection } from 'mongodb';
 import { Document } from 'bson/src/bson';
-import { WithId } from 'typeorm';
 import { EventsHandler, IEventHandler } from '@src/shared/domain/bus/eventBus/eventsHandler';
 import TermLikeAddedEvent from '@src/languages/domain/term/termLikeAddedEvent';
 import { MongoTransactional } from '@src/shared/infrastructure/persistence/mongo/mongoTransactionalDecorator';
@@ -11,11 +10,17 @@ import { MongoTransactional } from '@src/shared/infrastructure/persistence/mongo
 @MongoTransactional
 @EventsHandler(TermLikeAddedEvent)
 export default class AddLikeToTermViewProjectionHandler implements IEventHandler<TermLikeAddedEvent> {
-  constructor(@Inject(MONGO_CLIENT) private readonly mongo: MongoConnection) {}
+  private collection: Collection<Document>;
+
+  constructor(@Inject(MONGO_CLIENT) private readonly mongo: MongoConnection) {
+    this.collection = this.mongo.db.collection('terms');
+  }
 
   async handle(event: TermLikeAddedEvent): Promise<void> {
-    const termView = await this.getTerm(event.id);
+    const termView = await this.collection.findOne({ id: event.id });
+    if (!termView) throw new Error('Term view not found');
 
+    console.log(termView);
     if (termView.likes.some((like: TermLike) => like.userId === event.userId)) return;
 
     termView.totalLikes = termView.totalLikes + 1;
@@ -24,23 +29,12 @@ export default class AddLikeToTermViewProjectionHandler implements IEventHandler
     await this.save(termView as unknown as TermView);
   }
 
-  private async getTerm(id: string): Promise<WithId<Document>> {
-    const term = await this.collection().findOne({ id });
-    if (!term) throw new Error('Term view not found');
-
-    return term as unknown as WithId<Document>;
-  }
-
   private async save(termView: TermView): Promise<void> {
     const session = this.mongo.session;
     if (!session) {
       throw new Error('The session is not available');
     }
 
-    await this.collection().updateOne({ id: termView.id }, { $set: termView }, { session });
-  }
-
-  private collection(): Collection<Document> {
-    return this.mongo.db.collection('terms');
+    await this.collection.updateOne({ id: termView.id }, { $set: termView }, { session });
   }
 }
