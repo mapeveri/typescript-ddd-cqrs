ff141c6d80c277e0ce7a5b7f58852bdf0b0fb5a1
Martín Peveri <martinpeveri@gmail.com>
2024-07-29 19:39:28 +0200

    Refactor: findCountryQueryHandler.test.ts

--- test/unit/languages/application/country/query/findCountryQueryHandler.test.ts
+++ test/unit/languages/application/country/query/findCountryQueryHandler.test.ts
@@ -1,22 +1,39 @@
-import { beforeEach, describe, expect, it } from '@jest/globals';
+import { beforeEach, beforeAll, describe, expect, it } from '@jest/globals';
 import { FindCountryQueryMother } from './findCountryQueryMother';
 import FindCountryQueryHandler from '@src/languages/application/country/query/findCountryQueryHandler';
 import { CountryRepositoryMock } from '@test/unit/languages/domain/country/countryRepositoryMock';
 import { CountryIdMother } from '@test/unit/languages/domain/country/countryIdMother';
 import Country from '@src/languages/domain/country/country';
 import CountryMother from '@test/unit/languages/domain/country/countryMother';
+import FindCountryQuery from '@src/languages/application/country/query/findCountryQuery';
 
-describe('FindCountryQueryHandler', () => {
+describe('Given a FindCountryQueryHandler', () => {
   let countryRepository: CountryRepositoryMock;
   let findCountryQueryHandler: FindCountryQueryHandler;
 
-  beforeEach(() => {
+  const prepareDependencies = () => {
     countryRepository = new CountryRepositoryMock();
+  };
+
+  const initHandler = () => {
     findCountryQueryHandler = new FindCountryQueryHandler(countryRepository);
+  };
+
+  const clean = () => {
+    countryRepository.clean();
+  };
+
+  beforeAll(() => {
+    prepareDependencies();
+    initHandler();
   });
 
-  describe('execute', () => {
-    it('should get an empty result when the country does not exists', async () => {
+  beforeEach(() => {
+    clean();
+  });
+
+  describe('When the country does not exist', () => {
+    it('should get an empty result', async () => {
       const countryId = CountryIdMother.random();
       const query = FindCountryQueryMother.random(countryId.value);
 
@@ -24,12 +41,21 @@ describe('FindCountryQueryHandler', () => {
 
       expect(expected.content).toEqual(undefined);
     });
+  });
 
-    it('should get a country', async () => {
-      const country: Country = CountryMother.random();
-      const query = FindCountryQueryMother.random(country.id.value);
+  describe('When the country exists', () => {
+    let query: FindCountryQuery;
+    let country: Country;
+
+    function startScenario() {
+      country = CountryMother.random();
+      query = FindCountryQueryMother.random(country.id.value);
       countryRepository.add(country);
+    }
 
+    beforeEach(startScenario);
+
+    it('should get the country', async () => {
       const expected = await findCountryQueryHandler.execute(query);
 
       expect(expected.content).toEqual({
