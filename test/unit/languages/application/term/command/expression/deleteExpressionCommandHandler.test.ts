8c726585e2c1945a5ca5b73bc03d87a327dbcf2a
Martín Peveri <martinpeveri@gmail.com>
2024-08-06 21:56:36 +0200

    Improve test DeleteExpressionCommandHandler

--- test/unit/languages/application/term/command/expression/deleteExpressionCommandHandler.test.ts
+++ test/unit/languages/application/term/command/expression/deleteExpressionCommandHandler.test.ts
@@ -1,35 +1,95 @@
-import { beforeEach, describe, it } from '@jest/globals';
+import { beforeEach, beforeAll, describe, expect, it } from '@jest/globals';
 import DeleteExpressionCommandHandler from '@src/languages/application/term/command/expression/deleteExpressionCommandHandler';
 import { TermRepositoryMock } from '@test/unit/languages/domain/term/termRepositoryMock';
 import ExpressionMother from '@test/unit/languages/domain/term/expression/expressionMother';
 import { DeleteExpressionCommandMother } from '@test/unit/languages/application/term/command/expression/deleteExpressionCommandMother';
+import DeleteExpressionCommand from '@src/languages/application/term/command/expression/deleteExpressionCommand';
+import InvalidArgumentException from '@src/shared/domain/exceptions/invalidArgumentException';
+import Expression from '@src/languages/domain/term/expression/expression';
 
-describe('DeleteExpressionCommandHandler', () => {
+describe('Given a DeleteExpressionCommandHandler', () => {
   let termRepository: TermRepositoryMock;
-  let deleteExpressionCommandHandler: DeleteExpressionCommandHandler;
+  let handler: DeleteExpressionCommandHandler;
 
   beforeEach(() => {
     termRepository = new TermRepositoryMock();
 
-    deleteExpressionCommandHandler = new DeleteExpressionCommandHandler(termRepository);
+    handler = new DeleteExpressionCommandHandler(termRepository);
   });
 
-  describe('execute', () => {
-    it('should not remove when expression id does not exists', async () => {
-      const expression = ExpressionMother.random();
-      const command = DeleteExpressionCommandMother.random(expression.id.value);
+  const prepareDependencies = () => {
+    termRepository = new TermRepositoryMock();
+  };
 
-      await deleteExpressionCommandHandler.execute(command);
+  const initHandler = () => {
+    handler = new DeleteExpressionCommandHandler(termRepository);
+  };
+
+  const clean = () => {
+    termRepository.clean();
+  };
+
+  beforeAll(() => {
+    prepareDependencies();
+    initHandler();
+  });
+
+  beforeEach(() => {
+    clean();
+  });
+
+  describe('When the term id is invalid ', () => {
+    let command: DeleteExpressionCommand;
+
+    function startScenario() {
+      command = DeleteExpressionCommandMother.random('invalid');
+    }
+
+    beforeEach(startScenario);
+
+    it('then should thrown an exception', async () => {
+      await expect(handler.execute(command)).rejects.toThrowError(InvalidArgumentException);
+    });
+
+    it('then should not delete', async () => {
+      await expect(handler.execute(command)).rejects.toThrowError();
 
       termRepository.shouldNotRemove();
     });
+  });
 
-    it('should remove an expression', async () => {
+  describe('When the term does not exists ', () => {
+    let command: DeleteExpressionCommand;
+
+    function startScenario() {
       const expression = ExpressionMother.random();
-      const command = DeleteExpressionCommandMother.random(expression.id.value);
-      termRepository.add(expression);
+      command = DeleteExpressionCommandMother.random(expression.id.value);    
+    }
 
-      await deleteExpressionCommandHandler.execute(command);
+    beforeEach(startScenario);
+
+    it('then should not delete', async () => {
+      await handler.execute(command);
+
+      termRepository.shouldNotRemove();
+    });
+  });
+
+  describe('When the term exists ', () => {
+    let command: DeleteExpressionCommand;
+    let expression: Expression;
+
+    function startScenario() {
+      expression = ExpressionMother.random();
+      command = DeleteExpressionCommandMother.random(expression.id.value);
+      
+      termRepository.add(expression);
+    }
+
+    beforeEach(startScenario);
+
+    it('then should delete the term', async () => {
+      await handler.execute(command);
 
       termRepository.shouldRemove(expression);
     });
