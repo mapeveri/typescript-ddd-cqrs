2a43d246cfb6e756f4e110b964280d40506ddbb1
Martín Peveri <martinpeveri@gmail.com>
2024-08-10 14:35:45 +0200

    Improvements tests in FindSuggestionsTermQueryHandler

--- test/unit/languages/application/term/query/findSuggestionsTermQueryHandler.test.ts
+++ test/unit/languages/application/term/query/findSuggestionsTermQueryHandler.test.ts
@@ -1,33 +1,60 @@
-import { beforeEach, describe, expect, it } from '@jest/globals';
+import { beforeEach, beforeAll, describe, expect, it } from '@jest/globals';
 import { FindSuggestionsTermReadLayerMock } from '@test/unit/languages/application/term/query/findSuggestionsTermReadLayerMock';
 import FindSuggestionsTermQueryHandler from '@src/languages/application/term/query/findSuggestionsTermQueryHandler';
 import { FindSuggestionsTermQueryMother } from '@test/unit/languages/application/term/query/findSuggestionsTermQueryMother';
 import { TermViewMother } from '@test/unit/languages/application/term/query/view/termViewMother';
+import FindSuggestionsTermQuery from '@src/languages/application/term/query/findSuggestionsTermQuery';
+import { TermView } from '@src/languages/application/term/query/view/termView';
 
-describe('FindSuggestionsTermQueryHandler', () => {
-  let findSuggestionTermReadLayerMock: FindSuggestionsTermReadLayerMock;
-  let findSuggestionsTermQueryHandler: FindSuggestionsTermQueryHandler;
+describe('Given a FindSuggestionsTermQueryHandler', () => {
+  let findSuggestionTermReadLayer: FindSuggestionsTermReadLayerMock;
+  let handler: FindSuggestionsTermQueryHandler;
 
-  beforeEach(() => {
-    findSuggestionTermReadLayerMock = new FindSuggestionsTermReadLayerMock();
-    findSuggestionsTermQueryHandler = new FindSuggestionsTermQueryHandler(findSuggestionTermReadLayerMock);
+  const prepareDependencies = () => {
+    findSuggestionTermReadLayer = new FindSuggestionsTermReadLayerMock();
+  };
+
+  const initHandler = () => {
+    handler = new FindSuggestionsTermQueryHandler(findSuggestionTermReadLayer);
+  };
+
+  const clean = () => {
+    findSuggestionTermReadLayer.clean();
+  };
+
+  beforeAll(() => {
+    prepareDependencies();
+    initHandler();
   });
 
-  describe('execute', () => {
-    it('should get an empty result when no suggestion found', async () => {
+  beforeEach(() => {
+    clean();
+  });
+
+  describe('When no terms', () => {
+    it('should get an empty result', async () => {
       const query = FindSuggestionsTermQueryMother.random();
 
-      const foundSuggestionsTerm = await findSuggestionsTermQueryHandler.execute(query);
+      const foundSuggestionsTerm = await handler.execute(query);
 
       expect(foundSuggestionsTerm.content).toEqual([]);
     });
+  });
+
+  describe('Where there are terms', () => {
+    let query: FindSuggestionsTermQuery;
+    let term: TermView;
+
+    function startScenario() {
+      query = FindSuggestionsTermQueryMother.random();
+      term = TermViewMother.random();
+      findSuggestionTermReadLayer.add(term);
+    }
+
+    beforeEach(startScenario);
 
     it('should search suggestion term based on what the user wants to learn', async () => {
-      const query = FindSuggestionsTermQueryMother.random();
-      const term = TermViewMother.random();
-      findSuggestionTermReadLayerMock.add(term);
-
-      const foundSuggestionsTerm = await findSuggestionsTermQueryHandler.execute(query);
+      const foundSuggestionsTerm = await handler.execute(query);
 
       expect(foundSuggestionsTerm.content).toEqual([
         {
--- test/unit/languages/application/term/query/findSuggestionsTermReadLayerMock.ts
+++ test/unit/languages/application/term/query/findSuggestionsTermReadLayerMock.ts
@@ -1,14 +1,11 @@
-import { jest } from '@jest/globals';
 import { TermView } from '@src/languages/application/term/query/view/termView';
 import UserId from '@src/languages/domain/user/userId';
 import FindSuggestionsTermReadLayer from '@src/languages/application/term/query/findSuggestionsTermReadLayer';
 
 export class FindSuggestionsTermReadLayerMock implements FindSuggestionsTermReadLayer {
-  private readonly findMock: jest.Mock;
-  private readonly terms: TermView[];
+  private terms: TermView[];
 
   constructor() {
-    this.findMock = jest.fn();
     this.terms = [];
   }
 
@@ -16,8 +13,11 @@ export class FindSuggestionsTermReadLayerMock implements FindSuggestionsTermRead
     this.terms.push(term);
   }
 
-  async find(userId: UserId): Promise<TermView[]> {
-    this.findMock(userId);
-    return this.terms;
+  clean(): void {
+    this.terms = [];
+  }
+
+  async find(_userId: UserId): Promise<TermView[]> {
+    return Promise.resolve(this.terms);
   }
 }
