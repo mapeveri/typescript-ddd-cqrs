f6ec07b66e3f1cfd0472e6beb124d6b0294520f4
Martín Peveri <martinpeveri@gmail.com>
2024-08-15 12:45:12 +0200

    Unify event handler

--- src/languages/application/term/event/deleteTermOnTermCreatedUncompletedEventHandler.ts
+++ src/languages/application/term/event/deleteTermOnTermCreatedUncompletedEventHandler.ts
@@ -1,22 +1,16 @@
 import { COMMAND_BUS, CommandBus } from '@src/shared/domain/bus/commandBus/commandBus';
 import { Inject } from '@src/shared/domain/injector/inject.decorator';
 import { EventsHandler, IEventHandler } from '@src/shared/domain/bus/eventBus/eventsHandler';
-import TermType from '@src/languages/domain/term/termType';
 import TermCreatedUncompletedEvent from '@src/languages/domain/term/termCreatedUncompletedEvent';
 import DeleteTermCommand from '../command/deleteTermCommand';
 
 @EventsHandler(TermCreatedUncompletedEvent)
-export default class DeleteWordOnTermCreatedUncompletedEventHandler
+export default class DeleteTermOnTermCreatedUncompletedEventHandler
   implements IEventHandler<TermCreatedUncompletedEvent>
 {
   constructor(@Inject(COMMAND_BUS) private readonly commandBus: CommandBus) {}
 
   async handle(event: TermCreatedUncompletedEvent): Promise<void> {
-    const termType = TermType.of(event.type);
-    if (!termType.isWord()) {
-      return;
-    }
-
     await this.commandBus.dispatch(new DeleteTermCommand(event.aggregateId));
   }
 }
