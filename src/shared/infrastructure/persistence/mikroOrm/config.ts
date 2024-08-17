23c2e77d06150a0b40d22115b13cc0a7d929e90b
Martín Peveri <martinpeveri@gmail.com>
2024-08-17 19:06:24 +0200

    Fix configuration #142

--- src/shared/infrastructure/persistence/mikroOrm/config.ts
+++ src/shared/infrastructure/persistence/mikroOrm/config.ts
@@ -1,7 +1,12 @@
 import { Migrator } from '@mikro-orm/migrations';
 import { defineConfig, PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { entitySchemas } from '@src/shared/_dependencyInjection/entitySchemas';
+import { config as loadEnv } from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 import path from 'path';
+
+const env = loadEnv();
+dotenvExpand.expand(env);
 
 const migrationPath = '../../../../languages/app/mikroorm-migrations';
 
