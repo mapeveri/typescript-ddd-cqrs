9b7e2446335f73f9d2b65095ce5dd56a75edd489
Martín Peveri <martinpeveri@gmail.com>
2024-08-28 22:04:46 +0200

    Change auth session repository to use entity manager

--- src/languages/infrastructure/persistence/mikroOrm/repositories/mikroOrmAuthSessionRepository.ts
+++ src/languages/infrastructure/persistence/mikroOrm/repositories/mikroOrmAuthSessionRepository.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import AuthSession from '@src/languages/domain/auth/authSession';
 import { AuthSessionRepository } from '@src/languages/domain/auth/authSessionRepository';
-import { AuthSessionSchema } from '../entities/authSession';
-import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/core';
+import { EntityManager } from '@mikro-orm/core';
 
 @Injectable()
 export default class MikroOrmAuthSessionRepository implements AuthSessionRepository {
-  constructor(
-    @InjectRepository(AuthSessionSchema)
-    private readonly authSessionRepository: EntityRepository<AuthSession>,
-  ) {}
+  constructor(private readonly em: EntityManager) {}
 
   async save(authSession: AuthSession): Promise<any> {
-    return await this.authSessionRepository.upsert(authSession);
+    return await this.em.upsert(authSession);
   }
 }
