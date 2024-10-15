31972485f016ca1bf6457e0daf30724715e7ee5c
Martín Peveri <martinpeveri@gmail.com>
2024-10-15 19:31:11 +0200

    Move migrations files to mikroOrm folder

--- src/languages/infrastructure/persistence/mikroOrm/config.ts
+++ src/languages/infrastructure/persistence/mikroOrm/config.ts
@@ -8,7 +8,7 @@ import path from 'path';
 const env = loadEnv();
 dotenvExpand.expand(env);
 
-const migrationPath = path.join(__dirname, '../../../app/migrations');
+const migrationPath = path.join(__dirname, '/migrations');
 
 export default defineConfig({
   entities: entitySchemas,
