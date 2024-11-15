6c04869e9bc098965f44ca0f10415417c18687ed
Martín Peveri <martinpeveri@gmail.com>
2024-11-15 21:26:38 +0100

    Rename folder

--- src/languages/_dependencyInjection/readModels.ts
+++ src/languages/_dependencyInjection/readModels.ts
@@ -1,7 +1,7 @@
 import { FIND_SUGGESTIONS_TERM_READ_MODEL } from '@src/languages/application/term/query/findSuggestionsTermReadModel';
-import MongoFindSuggestionsTermReadModel from '@src/languages/infrastructure/persistence/mongo/readLayer/mongoFindSuggestionsTermReadModel';
+import MongoFindSuggestionsTermReadModel from '@src/languages/infrastructure/persistence/mongo/readModels/mongoFindSuggestionsTermReadModel';
 import { SEARCH_TERM_VIEW_READ_MODEL } from '@src/languages/application/term/query/searchTermViewReadModel';
-import MongoSearchTermViewReadModel from '@src/languages/infrastructure/persistence/mongo/readLayer/mongoSearchTermViewReadModel';
+import MongoSearchTermViewReadModel from '@src/languages/infrastructure/persistence/mongo/readModels/mongoSearchTermViewReadModel';
 
 export const readModels = [
   {
