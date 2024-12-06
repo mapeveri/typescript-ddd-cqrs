fcd5866a7aee09971253816d89ea9b556f36a7d5
Martín Peveri <martinpeveri@gmail.com>
2024-12-06 14:50:39 +0100

    Remove validation in UserId

--- src/languages/domain/user/invalidUserIdException.ts
+++
@@ -1,7 +0,0 @@
-import DomainException from '@src/shared/domain/exceptions/domainException';
-
-export default class InvalidUserIdException extends DomainException {
-  constructor(userId: string) {
-    super(`Invalid user id ${userId}`, 'invalid_user_id');
-  }
-}
--- src/languages/domain/user/userId.ts
+++ src/languages/domain/user/userId.ts
@@ -1,5 +1,4 @@
 import { Uuid } from '@src/shared/domain/valueObjects/uuid';
-import InvalidUserIdException from './invalidUserIdException';
 
 export default class UserId extends Uuid {
   static of(value: string): UserId {
@@ -8,12 +7,5 @@ export default class UserId extends Uuid {
 
   static fromPrimitives(value: string): UserId {
     return super.fromPrimitives(value) as UserId;
-  }
-
-  guardIsValid(email: string): void {
-    const userId = Uuid.fromString(email).toString();
-    if (userId !== this.value) {
-      throw new InvalidUserIdException(userId);
-    }
   }
 }
