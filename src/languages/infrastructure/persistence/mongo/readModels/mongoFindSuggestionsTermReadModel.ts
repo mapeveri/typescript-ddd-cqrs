6b9e8f7f7c71bd4faa08005995104b5dabe579b2
Martín Peveri <martinpeveri@gmail.com>
2024-12-08 22:47:42 +0100

    Remove user finder

--- src/languages/infrastructure/persistence/mongo/readModels/mongoFindSuggestionsTermReadModel.ts
+++ src/languages/infrastructure/persistence/mongo/readModels/mongoFindSuggestionsTermReadModel.ts
@@ -3,22 +3,19 @@ import UserId from '@src/languages/domain/user/userId';
 import { TermView } from '@src/languages/application/term/query/termView';
 import UserRepository, { USER_REPOSITORY } from '@src/languages/domain/user/userRepository';
 import { Inject } from '@src/shared/domain/injector/inject.decorator';
-import UserFinder from '@src/languages/domain/user/userFinder';
 import { Document } from 'mongodb';
 import MongoConnection, { MONGO_CLIENT } from '@src/shared/infrastructure/persistence/mongo/mongoConnection';
+import User from '@src/languages/domain/user/user';
+import UserDoesNotExistsException from '@src/languages/domain/user/userDoesNotExistsException';
 
 export default class MongoFindSuggestionsTermReadModel implements FindSuggestionsTermReadModel {
-  private readonly userFinder: UserFinder;
-
   constructor(
     @Inject(MONGO_CLIENT) private readonly mongo: MongoConnection,
-    @Inject(USER_REPOSITORY) userRepository: UserRepository,
-  ) {
-    this.userFinder = new UserFinder(userRepository);
-  }
+    @Inject(USER_REPOSITORY) private readonly userRepository: UserRepository,
+  ) {}
 
   async find(userId: UserId): Promise<TermView[]> {
-    const user = await this.userFinder.find(userId);
+    const user = await this.getUser(userId);
 
     const result = await this.mongo.db
       .collection('terms')
@@ -44,5 +41,14 @@ export default class MongoFindSuggestionsTermReadModel implements FindSuggestion
         createdAt: doc.createdAt,
       };
     });
+  }
+
+  async getUser(userId: UserId): Promise<User> {
+    const user = await this.userRepository.findById(userId);
+    if (null === user) {
+      throw new UserDoesNotExistsException(userId.toString());
+    }
+
+    return user;
   }
 }
