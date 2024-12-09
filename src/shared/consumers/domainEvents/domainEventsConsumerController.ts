77f884833185d84faa9488147331bd0e7b9c2d9b
Martín Peveri <martinpeveri@gmail.com>
2024-12-09 21:33:33 +0100

    Inject logger in DomainEventsConsumerController

--- src/shared/consumers/domainEvents/domainEventsConsumerController.ts
+++ src/shared/consumers/domainEvents/domainEventsConsumerController.ts
@@ -3,15 +3,20 @@ import { MessagePattern } from '@nestjs/microservices';
 import { Inject } from '@src/shared/domain/injector/inject.decorator';
 import { EVENT_BUS, EventBus } from '@src/shared/domain/bus/eventBus/eventBus';
 import { CreateRequestContext, MikroORM } from '@mikro-orm/core';
+import Logger, { LOGGER } from '@src/shared/domain/logger';
 
 @Controller()
 export class DomainEventsConsumerController {
-  constructor(private readonly orm: MikroORM, @Inject(EVENT_BUS) private readonly eventBus: EventBus) {}
+  constructor(
+    private readonly orm: MikroORM,
+    @Inject(EVENT_BUS) private readonly eventBus: EventBus,
+    @Inject(LOGGER) private readonly logger: Logger,
+  ) {}
 
   @MessagePattern('DomainEvents')
   @CreateRequestContext((self: DomainEventsConsumerController) => self.orm)
   async handleMessage(data: any): Promise<any> {
-    console.log(`Data received: ${JSON.stringify(data)}`);
+    this.logger.log(`[DomainEventsConsumerController]: Data received: ${JSON.stringify(data)}`);
 
     try {
       const classPath = `@src/${data.classPath}`;
@@ -20,12 +25,12 @@ export class DomainEventsConsumerController {
       const domainEventClass = require(filePath).default;
       const domainEvent = domainEventClass.fromPrimitives(data.message);
 
-      console.log(`Reacting to domain ${data.name}...`);
+      this.logger.log(`[DomainEventsConsumerController]: Reacting to domain ${data.name}...`);
       await this.eventBus.publish([domainEvent]);
 
-      console.log('Handled...');
+      this.logger.log('[DomainEventsConsumerController]: Handled...');
     } catch (e) {
-      console.error(e);
+      this.logger.error(`[DomainEventsConsumerController]: Error: ${e}`);
     }
 
     return;
