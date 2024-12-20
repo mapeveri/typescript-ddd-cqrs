85cfab56b1b3901c32be91a3db9c6fb64154cb02
Martín Peveri <martinpeveri@gmail.com>
2024-12-20 22:14:55 +0100

    Add token #172

--- src/languages/infrastructure/persistence/http/httpCollaboratorAdapter.ts
+++ src/languages/infrastructure/persistence/http/httpCollaboratorAdapter.ts
@@ -4,14 +4,28 @@ import { firstValueFrom } from 'rxjs';
 import CollaboratorId from '@src/languages/domain/collaborator/collaboratorId';
 import Collaborator from '@src/languages/domain/collaborator/collaborator';
 import { CollaboratorTranslator } from '@src/languages/infrastructure/persistence/http/collaboratorTranslator';
+import { Inject } from '@src/shared/domain/injector/inject.decorator';
+import { USER_AUTHENTICATOR, UserAuthenticator } from '@src/shared/domain/auth/userAuthenticator';
 
 @Injectable()
 export class HttpCollaboratorAdapter {
   URL = '/api/v1/users/{id}';
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject(USER_AUTHENTICATOR) private readonly userAuthenticator: UserAuthenticator,
+  ) {}
 
   async toCollaborator(id: CollaboratorId): Promise<Collaborator | null> {
-    const { data } = await firstValueFrom(this.httpService.get(this.URL.replace('{id}', id.toString())));
+    const m2mToken = this.userAuthenticator.sign({});
+
+    const { data } = await firstValueFrom(
+      this.httpService.get(this.URL.replace('{id}', id.toString()), {
+        headers: {
+          Authorization: `Bearer ${m2mToken.token}`,
+        },
+      }),
+    );
+
     if (!data) return null;
 
     return CollaboratorTranslator.toCollaborator(data);
