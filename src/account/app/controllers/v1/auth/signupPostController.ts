aa36f7bb91682ba998232ee4d41e425ca4b9c0e6
Martín Peveri <martinpeveri@gmail.com>
2025-01-18 16:27:56 +0100

    Modify SignupPostController to use identity provider #181

--- src/account/app/controllers/v1/auth/signupPostController.ts
+++ src/account/app/controllers/v1/auth/signupPostController.ts
@@ -3,19 +3,22 @@ import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiTags } from '
 import SignupPostDto from '@src/account/app/controllers/v1/auth/signupPostDto';
 import { COMMAND_BUS, CommandBus } from '@src/shared/domain/bus/commandBus/commandBus';
 import SignUpUserCommand from '@src/account/application/auth/command/signUpUserCommand';
-import { Uuid } from '@src/shared/domain/valueObjects/uuid';
+import { IDENTITY_PROVIDER, IdentityProvider } from '@src/shared/domain/services/IdentityProvider';
 
 @ApiTags('Auth')
 @Controller()
 export default class SignupPostController {
-  public constructor(@Inject(COMMAND_BUS) private commandBus: CommandBus) {}
+  public constructor(
+    @Inject(COMMAND_BUS) private commandBus: CommandBus,
+    @Inject(IDENTITY_PROVIDER) private identityProvider: IdentityProvider,
+  ) {}
 
   @Post('auth/signup')
   @HttpCode(201)
   @ApiBadRequestResponse({ description: 'Bad Request.' })
   @ApiInternalServerErrorResponse({ description: 'Internal Server Error.' })
   async run(@Body() payload: SignupPostDto): Promise<void> {
-    const id = Uuid.fromString(payload.email).toString();
+    const id = this.identityProvider.generateFromValue(payload.email);
 
     await this.commandBus.dispatch(
       new SignUpUserCommand(id, payload.name, payload.email, payload.token, payload.provider, payload.photo),
