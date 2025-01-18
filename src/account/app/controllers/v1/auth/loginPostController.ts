662afc350a3aa4c9a5ea6744a90128575203c203
Martín Peveri <martinpeveri@gmail.com>
2025-01-18 16:27:33 +0100

    Modify LoginPostController to use identity provider #181

--- src/account/app/controllers/v1/auth/loginPostController.ts
+++ src/account/app/controllers/v1/auth/loginPostController.ts
@@ -1,15 +1,18 @@
 import GetUserSocialLoginQuery from '@src/account/application/auth/query/getUserSocialLoginQuery';
-import { Uuid } from '@src/shared/domain/valueObjects/uuid';
 import { Body, Controller, HttpCode, Inject, Post } from '@nestjs/common';
 import LoginPostDto from './loginPostDto';
 import LoginPostResponseDto from './loginPostResponseDto';
 import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { QUERY_BUS, QueryBus } from '@src/shared/domain/bus/queryBus/queryBus';
+import { IDENTITY_PROVIDER, IdentityProvider } from '@src/shared/domain/services/IdentityProvider';
 
 @ApiTags('Auth')
 @Controller()
 export default class LoginPostController {
-  public constructor(@Inject(QUERY_BUS) private queryBus: QueryBus) {}
+  public constructor(
+    @Inject(QUERY_BUS) private queryBus: QueryBus,
+    @Inject(IDENTITY_PROVIDER) private identityProvider: IdentityProvider,
+  ) {}
 
   @Post('auth/login')
   @HttpCode(200)
@@ -17,7 +20,7 @@ export default class LoginPostController {
   @ApiBadRequestResponse({ description: 'Bad Request.' })
   @ApiInternalServerErrorResponse({ description: 'Internal Server Error.' })
   async run(@Body() payload: LoginPostDto): Promise<LoginPostResponseDto> {
-    const id = Uuid.fromString(payload.email).toString();
+    const id = this.identityProvider.generateFromValue(payload.email);
 
     const response = await this.queryBus.ask(
       new GetUserSocialLoginQuery(id, payload.name, payload.email, payload.token, payload.provider, payload.photo),
