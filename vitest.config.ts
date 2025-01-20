9de7e91aa21d530ab3a53028ef7c8e2c86749a31
Martín Peveri <martinpeveri@gmail.com>
2025-01-20 19:54:57 +0100

    Initial configuration to migrate from jest to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    setupFiles: './test/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './coverage',
+    },
+  },
+});
