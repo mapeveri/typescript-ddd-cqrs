a26756df62e3b22976f8e9248dbe1ebbd1284b4c
Martín Peveri <martinpeveri@gmail.com>
2025-01-31 15:54:03 +0100

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,11 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     setupFiles: './test/setup.ts',
-    include: ['test/unit/**/*.test.ts'],
+    include: ['test/unit/**/*.test.ts', 'test/acceptance/**/*.test.ts'],
+    alias: {
+      '@src': '/src',
+      '@test': '/test',
+    },
     coverage: {
       provider: 'v8',
       reportsDirectory: './coverage',
