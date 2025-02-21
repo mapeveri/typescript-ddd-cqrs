c16b884433c6f0e1b237921912f85dcd72cd7e1e
Martín Peveri <martinpeveri@gmail.com>
2025-02-21 18:18:33 +0100

    Add unhandledRejection #187

--- test/setup.ts
+++ test/setup.ts
@@ -4,3 +4,7 @@ process.env.NODE_ENV = 'test';
 
 const envTestPath = `${path.dirname(__dirname)}/.env.test`;
 require('dotenv').config({ path: envTestPath });
+
+process.on('unhandledRejection', (reason) => {
+  console.warn('Unhandled rejection detected:', reason);
+});
