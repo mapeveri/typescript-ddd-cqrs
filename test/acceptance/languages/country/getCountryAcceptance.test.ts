a30926e33d5ef0812fe2050826fc4b73a6ac15eb
Martín Peveri <martinpeveri@gmail.com>
2025-03-01 17:56:20 +0100

    Update test getCountryAcceptance.test.ts

--- test/acceptance/languages/country/getCountryAcceptance.test.ts
+++ test/acceptance/languages/country/getCountryAcceptance.test.ts
@@ -8,6 +8,7 @@ import { CountryPrimitives } from '@src/languages/domain/country/country';
 import { CountryIdMother } from '@test/unit/languages/domain/country/countryIdMother';
 
 describe('Get country feature', () => {
+  const COUNTRY_ID = 'e625816c-2117-44a6-a0ba-07fe54cb0cc4';
   let app: INestApplication;
   let orm: MikroORM;
 
@@ -32,18 +33,19 @@ describe('Get country feature', () => {
 
   describe('As a user I want to get a country', () => {
     let countryData: CountryPrimitives;
-    const countryId = 'e625816c-2117-44a6-a0ba-07fe54cb0cc4';
 
     async function startScenario() {
       await truncateTables(orm);
 
-      const country = CountryMother.random({ id: CountryIdMother.random(countryId) });
+      const country = CountryMother.random({ id: CountryIdMother.random(COUNTRY_ID) });
 
       countryData = country.toPrimitives();
       await request(app.getHttpServer()).post('/countries').set('Authorization', 'Bearer mock-token').send(countryData);
     }
 
-    beforeEach(startScenario);
+    beforeEach(async () => {
+      await startScenario();
+    });
 
     it('should return an empty object when it does not exists', async () => {
       await request(app.getHttpServer())
@@ -55,7 +57,7 @@ describe('Get country feature', () => {
 
     it('should return the country when it exists', async () => {
       await request(app.getHttpServer())
-        .get(`/countries/${countryId}`)
+        .get(`/countries/${COUNTRY_ID}`)
         .set('Authorization', 'Bearer mock-token')
         .expect(200)
         .expect(countryData);
