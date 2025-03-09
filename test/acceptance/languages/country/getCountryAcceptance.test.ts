dfd8ae18fb64334a22b40c6bcc6734acaad57733
Martín Peveri <martinpeveri@gmail.com>
2025-03-09 13:35:44 +0100

    Fix getCountryAcceptance.test.ts

--- test/acceptance/languages/country/getCountryAcceptance.test.ts
+++ test/acceptance/languages/country/getCountryAcceptance.test.ts
@@ -1,16 +1,24 @@
-import { beforeAll, describe, beforeEach, afterAll, it } from 'vitest';
+import { beforeAll, describe, beforeEach, afterAll, it, expect } from 'vitest';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { MikroORM } from '@mikro-orm/core';
-import { createApplication, truncateTables } from '@test/acceptance/createApplication';
+import { createApplication } from '@test/acceptance/createApplication';
 import CountryMother from '@test/unit/languages/domain/country/countryMother';
 import { CountryPrimitives } from '@src/languages/domain/country/country';
 import { CountryIdMother } from '@test/unit/languages/domain/country/countryIdMother';
+import LanguageMother from '@test/unit/languages/domain/country/languageMother';
 
 describe('Get country feature', () => {
-  const COUNTRY_ID = 'e625816c-2117-44a6-a0ba-07fe54cb0cc4';
   let app: INestApplication;
   let orm: MikroORM;
+
+  const COUNTRY_ID = 'e625816c-2117-44a6-a0ba-07fe54cb0cc4';
+  const NAME = 'Spain';
+  const ISO = 'ES';
+  const LANGUAGE = {
+    languageId: 'EN',
+    name: 'Spanish',
+  };
 
   const prepareApp = async () => {
     const setup = await createApplication();
@@ -35,9 +43,12 @@ describe('Get country feature', () => {
     let countryData: CountryPrimitives;
 
     async function startScenario() {
-      await truncateTables(orm);
-
-      const country = CountryMother.random({ id: CountryIdMother.random(COUNTRY_ID) });
+      const country = CountryMother.random({
+        id: CountryIdMother.random(COUNTRY_ID),
+        name: NAME,
+        iso: ISO,
+        languages: [LanguageMother.random(LANGUAGE)],
+      });
 
       countryData = country.toPrimitives();
       await request(app.getHttpServer()).post('/countries').set('Authorization', 'Bearer mock-token').send(countryData);
@@ -56,11 +67,17 @@ describe('Get country feature', () => {
     });
 
     it('should return the country when it exists', async () => {
-      await request(app.getHttpServer())
+      const response = await request(app.getHttpServer())
         .get(`/countries/${COUNTRY_ID}`)
         .set('Authorization', 'Bearer mock-token')
-        .expect(200)
-        .expect(countryData);
+        .expect(200);
+
+      expect(response.body).toEqual({
+        id: countryData.id,
+        name: countryData.name,
+        languages: countryData.languages,
+        iso: countryData.iso,
+      });
     });
   });
 });
