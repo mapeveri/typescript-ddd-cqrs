c92530af494c257ea55e9f0b233086a714230ac6
Martín Peveri <martinpeveri@gmail.com>
2025-03-27 21:41:57 +0100

    Add new mikro-orm connection to account module #188

---
+++ src/account/mikroOrmConfig.ts
@@ -0,0 +1,36 @@
+import { Migrator } from '@mikro-orm/migrations';
+import { defineConfig, PostgreSqlDriver } from '@mikro-orm/postgresql';
+
+import { config as loadEnv } from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+import path from 'path';
+import { entitySchemas as accountEntitySchemas } from '@src/account/_dependencyInjection/entitySchemas';
+
+const env = loadEnv();
+dotenvExpand.expand(env);
+
+const migrationPath = path.join(__dirname, 'infrastructure/persistence/mikroOrm/migrations');
+const entitySchemas = [...accountEntitySchemas];
+
+export const mikroOrmConfiguration = {
+  name: 'account',
+  entities: entitySchemas,
+  entitiesTs: entitySchemas,
+  driver: PostgreSqlDriver,
+  clientUrl: process.env.POSTGRESQL_DB_URL,
+  debug: process.env.ENV != 'production',
+  extensions: [Migrator],
+  ignoreUndefinedInQuery: true,
+  forceUndefined: false,
+  migrations: {
+    path: migrationPath,
+    glob: '{account}/*.{js,ts}',
+    pathTs: migrationPath,
+    tableName: 'mikro_orm_migrations',
+    transactional: true,
+    allOrNothing: true,
+    snapshot: false,
+  },
+};
+
+export default defineConfig(mikroOrmConfiguration);
