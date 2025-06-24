76c96b558224f38b09d859eb00f88c9c31496c65
Martín Peveri <martinpeveri@gmail.com>
2025-06-24 13:49:20 +0200

    Add override to setup #199

--- test/setup.ts
+++ test/setup.ts
@@ -3,7 +3,7 @@ import path from 'path';
 process.env.NODE_ENV = 'test';
 
 const envTestPath = `${path.dirname(__dirname)}/.env.test`;
-require('dotenv').config({ path: envTestPath });
+require('dotenv').config({ path: envTestPath, override: true });
 
 process.on('unhandledRejection', (reason) => {
   console.warn('Unhandled rejection detected:', reason);
